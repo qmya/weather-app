c0e52d3afc333866c64d28ec142f81d508c7b207
qmya <qandeelgul90@gmail.com>
2020-09-02 11:27:12 -0400

    created Form to enter the name of city and country and get the weather

---
+++ src/components/Form/Form.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+const Form = (props) => {
+    return(
+        <div className="container">
+            <div className="row">
+               <div className="col-md-3">
+                   <input type="text" className="form-control" name="city" autoComplete="off"/>
+                </div> 
+                <div className="col-md-3">
+                    <input type="text" className="form-control" name="city" autoComplete="off"/>
+                </div>
+                <div className="col-md-3">
+                    <button className="btn-warning"> Get Weather </button>
+                </div>
+            </div>
+
+
+        </div>
+    );
+}
+
+export default Form;
